fa2d6e063ec3ae2a7872daf95c3590f3c8a12c7c
poko8nada <57497035+poko8nada@users.noreply.github.com>
2026-02-12 15:22:58 +0900

    update:docs and header to response

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,33 +4,37 @@ import { TextLink } from './text-link'
 export const Header: FC = () => {
   return (
     <header class='mt-2 px-1 fixed max-w-2xl w-full left-1/2 -translate-x-1/2 z-50'>
-      <div class='flex items-center justify-between pl-3 pr-10 py-3 border border-text-primary rounded-full bg-background shadow-sm'>
-        <div class='flex items-center gap-5'>
-          <div class='w-16 h-16 rounded-full overflow-hidden shrink-0 border border-text-primary/10'>
+      <div class='flex items-center justify-between pl-3 pr-6 sm:pr-10 py-3 border border-text-primary rounded-full bg-background shadow-sm'>
+        <div class='flex items-center gap-2 sm:gap-5'>
+          <div class='w-12 sm:w-16 h-12 sm:h-16 rounded-full overflow-hidden shrink-0 border border-text-primary/10'>
             <img
               src='/face.jpeg'
               alt='Poko Hanada'
               class='w-full h-full object-cover'
             />
           </div>
-          <a href='/' class='text-text-primary text-2xl tracking-tighter'>
-            Poko Hanada
+          <a
+            href='/'
+            class='text-text-primary text-lg sm:text-2xl tracking-tighter mr-2'
+          >
+            Poko <br class='sm:hidden' />
+            Hanada
           </a>
         </div>
         <nav>
-          <ul class='flex gap-8'>
+          <ul class='flex gap-2 sm:gap-6 space-x-1'>
             <li>
-              <TextLink href='/#about' class='text-lg font-medium'>
+              <TextLink href='/#about' class='text-md sm:text-lg font-medium'>
                 About
               </TextLink>
             </li>
             <li>
-              <TextLink href='/#posts' class='text-lg font-medium'>
+              <TextLink href='/#posts' class='text-md sm:text-lg font-medium'>
                 Posts
               </TextLink>
             </li>
             <li>
-              <TextLink href='/#tools' class='text-lg font-medium'>
+              <TextLink href='/#tools' class='text-md sm:text-lg font-medium'>
                 Tools
               </TextLink>
             </li>
